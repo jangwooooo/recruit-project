36e687d538af209840f379c0e777b559b7a33857
jangwooooo <s22018@gsm.hs.kr>
2023-01-12 08:27:56 +0900

    TokenNotValid exception

--- src/main/java/com/example/demo/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/global/exception/handler/GlobalExceptionHandler.java
@@ -3,10 +3,10 @@ package com.example.demo.global.exception.handler;
 import com.example.demo.global.exception.ErrorCode;
 import com.example.demo.global.exception.ErrorResponse;
 import com.example.demo.global.exception.exceptionCollection.TokenExpirationException;
+import com.example.demo.global.exception.exceptionCollection.TokenNotValidException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,9 +20,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(TokenExpirationException.class)
     public ResponseEntity<ErrorResponse> TokenExpirationException(TokenExpirationException e, HttpServletRequest request) {
         log.warn("TokenExpirationException 발생!!! url:{}, trace:{}", request.getRequestURI(), e.getStackTrace());
-        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.NOT_NULL.getMessage(), ErrorCode.NOT_NULL.getStatus());
-        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
+    @ExceptionHandler(TokenNotValidException.class)
+    public ResponseEntity<ErrorResponse> TokenNotValidException(TokenNotValidException e, HttpServletRequest request) {
+        log.warn("TokenNotValidException 발생!!! url:{}, trace:{}", request.getRequestURI(), e.getStackTrace());
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
 }
 
