324adad7854c6e56ed3085848e1a979615497d5f
jangwooooo <s22018@gsm.hs.kr>
2023-01-16 12:24:40 +0900

    requestmapping 제거

--- src/main/java/com/example/demo/domain/auth/presentation/TokenController.java
+++ src/main/java/com/example/demo/domain/auth/presentation/TokenController.java
@@ -5,19 +5,15 @@ import com.example.demo.domain.auth.service.impl.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/token")
 @RestController
 public class TokenController {
 
     private final MemberServiceImpl memberService;
 
-    @GetMapping("/reissue")
+    @PatchMapping("/token/reissue")
     public ResponseEntity<NewTokenResponse> reIssueToken(@RequestHeader("RefreshToken") String refreshToken) {
         NewTokenResponse reIssueToken = memberService.tokenReissuance(refreshToken);
         return new ResponseEntity<>(reIssueToken, HttpStatus.OK);
