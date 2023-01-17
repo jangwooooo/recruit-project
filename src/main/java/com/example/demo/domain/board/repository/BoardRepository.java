e519df64fe0541c993458d0ac08448d463b09f23
jangwooooo <s22018@gsm.hs.kr>
2023-01-17 19:27:30 +0900

    findBoardsByAuthor 생성

--- src/main/java/com/example/demo/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/demo/domain/board/repository/BoardRepository.java
@@ -3,8 +3,10 @@ package com.example.demo.domain.board.repository;
 import com.example.demo.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findBoardsByAuthor(String name);
 }
