05eb67b9514cf1f31b0b85dcc4e23dd010b62368
jangwooooo <s22018@gsm.hs.kr>
2023-01-17 19:18:52 +0900

    EditBoardReq 패키지 생성

---
+++ src/main/java/com/example/demo/domain/board/presentation/dto/reqeust/EditBoardReq.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.board.presentation.dto.reqeust;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditBoardReq {
+
+    @NotNull
+    private Long boardId;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String content;
+    @NotBlank
+    private String type;
+    @NotBlank
+    private String reqruit;
+    @NotBlank
+    private String contactType;
+    @NotBlank
+    private String contactUs;
+    private LocalDate endDate;
+}
