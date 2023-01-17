8f37934fe06d2dab239a63dffab0e8d5ba45efbb
jangwooooo <s22018@gsm.hs.kr>
2023-01-17 17:27:08 +0900

    board entity 생성

--- src/main/java/com/example/demo/domain/board/entity/Board.java
+++ src/main/java/com/example/demo/domain/board/entity/Board.java
@@ -1,4 +1,33 @@
 package com.example.demo.domain.board.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+    private String title;
+    private String content;
+    private String type;
+    private String reqruit;
+    private String contactType;
+    private String contactUs;
+    private String author;
+    private LocalDate endDate;
 }
