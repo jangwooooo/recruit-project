7c5ce55d2dd022790c2900737a0bb130a65ccdd6
jangwooooo <s22018@gsm.hs.kr>
2023-01-28 19:26:32 +0900

    BoardResponse dto생성

---
+++ src/main/java/com/example/demo/domain/board/presentation/dto/response/BoardResponse.java
@@ -0,0 +1,24 @@
+package com.example.demo.domain.board.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardResponse {
+
+    private Long boardId;
+    private String author;
+    private String title;
+    private String content;
+    private String category;
+    private String recruit;
+    private LocalDate endDate;
+    private LocalDate createdAt;
+}
