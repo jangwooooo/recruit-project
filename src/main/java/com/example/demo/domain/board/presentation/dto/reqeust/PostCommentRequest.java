0be2cf0a442bbe43c4105a04481c9f725a1dddbf
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 00:10:59 +0900

    @Getter, 생성자어노테이션 추가

--- src/main/java/com/example/demo/domain/board/presentation/dto/reqeust/PostCommentRequest.java
+++ src/main/java/com/example/demo/domain/board/presentation/dto/reqeust/PostCommentRequest.java
@@ -1,5 +1,11 @@
 package com.example.demo.domain.board.presentation.dto.reqeust;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostCommentRequest {
 }
