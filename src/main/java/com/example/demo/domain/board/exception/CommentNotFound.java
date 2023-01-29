5745b3628ab6326d6e5f771b41541fcea22806a1
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 08:54:47 +0900

    CommentNotFound 구현

--- src/main/java/com/example/demo/domain/board/exception/CommentNotFound.java
+++ src/main/java/com/example/demo/domain/board/exception/CommentNotFound.java
@@ -1,4 +1,15 @@
 package com.example.demo.domain.board.exception;
 
-public class CommentNotFound {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CommentNotFound extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+    public CommentNotFound(String message) {
+        super(message);
+        this.errorCode = ErrorCode.COMMENT_NOT_FOUND;
+    }
 }
