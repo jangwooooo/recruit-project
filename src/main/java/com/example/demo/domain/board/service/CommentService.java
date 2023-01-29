e1addefbec95515897276a7ed73ccef8045a938c
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 00:30:11 +0900

    CommentService post메서드 구현

--- src/main/java/com/example/demo/domain/board/service/CommentService.java
+++ src/main/java/com/example/demo/domain/board/service/CommentService.java
@@ -1,9 +1,14 @@
 package com.example.demo.domain.board.service;
 
+import com.example.demo.domain.board.entity.Comment;
+import com.example.demo.domain.board.presentation.dto.reqeust.PostCommentRequest;
 import com.example.demo.domain.board.repository.CommentRepository;
+import com.example.demo.domain.user.entity.User;
 import com.example.demo.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +16,16 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final UserUtil userUtil;
+
+    @Transactional
+    public void post(PostCommentRequest request) {
+        User user = userUtil.currentUser();
+        Comment comment = Comment.builder()
+                .boardId(request.getBoardId())
+                .writer(user.getName())
+                .content(request.getContent())
+                .build();
+
+        commentRepository.save(comment);
+    }
 }
