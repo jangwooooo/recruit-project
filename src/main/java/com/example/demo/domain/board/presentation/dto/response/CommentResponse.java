5f5fb340f8a8f24b70f6823999b7b8b1615fdf61
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 00:50:54 +0900

    어노테이션 추가

--- src/main/java/com/example/demo/domain/board/presentation/dto/response/CommentResponse.java
+++ src/main/java/com/example/demo/domain/board/presentation/dto/response/CommentResponse.java
@@ -1,4 +1,13 @@
 package com.example.demo.domain.board.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentResponse {
 }
