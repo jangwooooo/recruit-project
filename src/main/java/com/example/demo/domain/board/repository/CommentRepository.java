fdc678dae1ec8bff87c38786bf82d4086da2a489
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 00:17:58 +0900

    JpaRepository 상속

--- src/main/java/com/example/demo/domain/board/repository/CommentRepository.java
+++ src/main/java/com/example/demo/domain/board/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.board.repository;
 
-public interface CommentRepository {
+import com.example.demo.domain.board.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
