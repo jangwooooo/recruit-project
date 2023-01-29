21f7074378feeb1f7e3ecfcc44a3757ed0cefc58
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 01:19:58 +0900

    게시글이 존재하지 않을때 예외처리

--- src/main/java/com/example/demo/domain/board/service/CommentService.java
+++ src/main/java/com/example/demo/domain/board/service/CommentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.domain.board.service;
 
 import com.example.demo.domain.board.entity.Comment;
+import com.example.demo.domain.board.exception.BoardNotFoundException;
 import com.example.demo.domain.board.presentation.dto.reqeust.PostCommentRequest;
 import com.example.demo.domain.board.presentation.dto.response.BoardListResponse;
 import com.example.demo.domain.board.presentation.dto.response.CommentResponse;
+import com.example.demo.domain.board.repository.BoardRepository;
 import com.example.demo.domain.board.repository.CommentRepository;
 import com.example.demo.domain.user.entity.User;
 import com.example.demo.global.util.UserUtil;
@@ -20,6 +22,7 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final UserUtil userUtil;
+    private final BoardRepository boardRepository;
 
     @Transactional
     public void post(PostCommentRequest request) {
@@ -35,6 +38,8 @@ public class CommentService {
 
     @Transactional
     public List<CommentResponse> getComments(Long boardId) {
+        boardRepository.findById(boardId)
+                .orElseThrow(() -> new BoardNotFoundException("게시글을 찾을 수 없습니다."));
         return commentRepository.findCommentsByBoardId(boardId).stream()
                 .map(CommentResponse::new)
                 .collect(Collectors.toList());
