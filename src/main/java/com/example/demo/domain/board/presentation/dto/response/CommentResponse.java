3134f1c173fbec1aab79be513a55599edb63e235
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 00:52:43 +0900

    변수 추가

--- src/main/java/com/example/demo/domain/board/presentation/dto/response/CommentResponse.java
+++ src/main/java/com/example/demo/domain/board/presentation/dto/response/CommentResponse.java
@@ -5,9 +5,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentResponse {
+
+    private Long commentId;
+    private Long boardId;
+    private String writer;
+    private String content;
+    private LocalDate createdAt;
 }
