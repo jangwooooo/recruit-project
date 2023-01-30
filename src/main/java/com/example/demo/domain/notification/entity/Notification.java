3c52eb72b09990f1a44e6225bfb1e2c4f8c58db2
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 15:49:06 +0900

    Nofication클래스 구현

--- src/main/java/com/example/demo/domain/notification/entity/Notification.java
+++ src/main/java/com/example/demo/domain/notification/entity/Notification.java
@@ -1,14 +1,25 @@
 package com.example.demo.domain.notification.entity;
 
+import com.example.demo.global.entity.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Notification {
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificationId;
+    private Long boardId;
+    private String generatedUser;
+    private Boolean isRead;
 }
