bc253a889056377b73238915d75db9d0779a3986
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 16:22:15 +0900

    알림목록 조회 컨틀롤러 구현

--- src/main/java/com/example/demo/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/example/demo/domain/notification/presentation/NotificationController.java
@@ -1,8 +1,13 @@
 package com.example.demo.domain.notification.presentation;
 
+import com.example.demo.domain.notification.presentation.response.NotificationResponse;
 import com.example.demo.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/notification")
 public class NotificationController {
 
+    private final NotificationService notificationService;
+
+    @GetMapping
+    public ResponseEntity<List<NotificationResponse>> getNotifications() {
+        List<NotificationResponse> responses = notificationService.getList();
+        return new ResponseEntity<>(responses, HttpStatus.OK);
+    }
 }
