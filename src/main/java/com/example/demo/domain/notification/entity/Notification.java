b79027b4d33a491ff5d1ba7385880ddd53260d14
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 15:24:44 +0900

    entity, getter, noargs, allargs 어노테이션 추가

--- src/main/java/com/example/demo/domain/notification/entity/Notification.java
+++ src/main/java/com/example/demo/domain/notification/entity/Notification.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.notification.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notification {
 }
