b158c0f912a051b4673aae58ee3eb1d3cd6a1d14
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 16:15:53 +0900

    NotificationController 생성

---
+++ src/main/java/com/example/demo/domain/notification/presentation/NotificationController.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.notification.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notification")
+public class NotificationController {
+
+
+}
