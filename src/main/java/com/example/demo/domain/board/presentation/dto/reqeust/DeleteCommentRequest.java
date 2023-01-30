98ac83c224c8ffab75d501e43a48b390f4da23a7
jangwooooo <s22018@gsm.hs.kr>
2023-01-30 10:28:56 +0900

    commentId, boardId 변수추가

--- src/main/java/com/example/demo/domain/board/presentation/dto/reqeust/DeleteCommentRequest.java
+++ src/main/java/com/example/demo/domain/board/presentation/dto/reqeust/DeleteCommentRequest.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.board.presentation.dto.reqeust;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeleteCommentRequest {
+
+    private Long commentId;
+    private Long boardId;
 }
