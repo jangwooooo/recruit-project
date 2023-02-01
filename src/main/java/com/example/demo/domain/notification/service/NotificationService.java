663e6f30727ad67bf81da93cc8f5fc5360646e29
jangwooooo <s22018@gsm.hs.kr>
2023-02-01 23:58:02 +0900

    improt 제거

--- src/main/java/com/example/demo/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/demo/domain/notification/service/NotificationService.java
@@ -1,6 +1,5 @@
 package com.example.demo.domain.notification.service;
 
-import com.example.demo.domain.board.presentation.dto.response.BoardListResponse;
 import com.example.demo.domain.board.repository.BoardRepository;
 import com.example.demo.domain.notification.exception.NotificationNotFound;
 import com.example.demo.domain.notification.presentation.response.NotificationResponse;
